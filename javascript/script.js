37408e89a47f19de19b7bb2ce78480fb1f0b18f3
Franck <franckthev@live.fr>
2019-02-25 20:04:24 +0100

    v2

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,6 @@
-$(".menu-collapsed").click(function() {
-    $(this).toggleClass("menu-expanded");
-  });
+var btn = document.querySelector('.toggle_btn');
+var nav = document.querySelector('.nav');
+
+btn.onclick = function(){
+  nav.classList.toggle('nav_open');
+}
